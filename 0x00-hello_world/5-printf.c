f6e264a3a9f5f4c937385d39fa866c3cbb272e86
ProbablyOmar <omar_ahmedwassim@hotmail.com>
2022-12-09 08:04:39 -0800

    Improved style

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,5 +4,5 @@
 int main(void)
 {
 	prinf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0)
+	return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-
+/*Entry Point*/
 int main(void)
 {
-	printf("Size of a char: %ld byte(s)\n",sizeof(char));
-	printf("Size of an int: %ld byte(s)\n",sizeof(int));
-	printf("Size of a long int: %ld byte(s)\n",sizeof(long));
-	printf("Size of a long long int: %ld byte(s)\n",sizeof(long long));
-	printf("Size of a float: %ld byte(s)\n",sizeof(float));
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of an int: %ld byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long));
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
 
-	return(0);
+	return (0);
 }
