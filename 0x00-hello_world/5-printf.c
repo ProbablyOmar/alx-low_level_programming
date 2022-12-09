21a7e39c4768bd59f6a5e66b0e5286dad9986491
ProbablyOmar <omar_ahmedwassim@hotmail.com>
2022-12-09 07:40:55 -0800

    Added new scripts

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+/*Entry Point*/
+int main(void)
+{
+	prinf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0)
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Size of a char: %d byte(s)
+
+			Size of an int: %d byte(s)
+
+			Size of a long int: %d byte(s)
+
+			Size of a long long int: %d byte(s)
+
+			Size of a float: %d byte(s)", sizeof(char), sizeof(int), sizeof(long), sizeof(long long), sizeof(float))
+	return(0)
+}
