e0888fd6062553b79276ad420c4ae5783c975285
ProbablyOmar <omar_ahmedwassim@hotmail.com>
2022-12-09 16:25:17 -0800

    More bugs

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -16,6 +16,6 @@ int main(void)
 			putchar(i);
 	}
 
-	putchar(0);
+	putchar(10);
 	return (0);
 }
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,12 +9,15 @@ int main(void)
 {
 	int i;
 
-	for (i = 48; i < 57; i++)
+	for (i = 48; i < 58; i++)
 	{
 		putchar(i);
-		putchar(44);
-		putchar(32);
+		if (i != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
 	}
-	putchar(57);
+	putchar(10);
 	return (0);
 }
