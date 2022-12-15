442117df8c76d81ba44b308598e1e5f99289c905
ProbablyOmar <omar_ahmedwassim@hotmail.com>
2022-12-15 10:44:58 -0800

    Largest number program

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,5 +1,3 @@
-#include "main.h"
-
 /**
  * largest_number - returns the largest of 3 numbers
  * @a: first integer
@@ -7,10 +5,13 @@
  * @c: third integer
  * Return: largest number
  */
+
 int largest_number(int a, int b, int c)
 {
 	int largest;
 
 	largest = a > b? a : b;
 	largest = largest > c? largest : c;
+
+	return (largest);
 }
