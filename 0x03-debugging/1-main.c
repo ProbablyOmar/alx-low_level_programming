7da2a6eaf6c7fd1339d1d8996849add1772ce05d
ProbablyOmar <omar_ahmedwassim@hotmail.com>
2022-12-15 06:24:20 -0800

    First debugging problem

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-* main - causes an infinite loop
+* main - prevents a infinite loop
 * Return: 0
 */
 
@@ -11,9 +11,9 @@ int main(void)
 
 	printf("Infinite loop incoming :(\n");
 
-	i = 0;
+	/*i = 0;
 
-	/*while (i < 10)
+	while (i < 10)
 	{
 		putchar(i);
 	}*/
