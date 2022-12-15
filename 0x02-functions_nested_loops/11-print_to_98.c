4d1302cc651abb85e80c282997b490253004b130
ProbablyOmar <omar_ahmedwassim@hotmail.com>
2022-12-15 03:38:57 -0800

    Source code for more programs

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,7 +13,7 @@ void print_to_98(int n)
 		while (n != 98)
 		{
 			printf("%d, ", n);
-			n--
+			n--;
 		}
 	}
 	else
@@ -21,7 +21,7 @@ void print_to_98(int n)
 		while (n != 98)
 		{
 			printf("%d, ", n);
-			n++
+			n++;
 		}
 	}
 	printf("98");
