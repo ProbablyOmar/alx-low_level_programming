8673018250ba3968973b4e5e02b4e0885107e8de
ProbablyOmar <omar_ahmedwassim@hotmail.com>
2022-12-15 10:58:04 -0800

    Fixed style

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,7 +11,7 @@ int largest_number(int a, int b, int c)
 	int largest;
 
 	largest = a > b ? a : b;
-	largest = largest > c? largest : c;
+	largest = largest > c ? largest : c;
 
 	return (largest);
 }
