deda2827be94e4ccdb9022c086fa2c19802ae112
ProbablyOmar <omar_ahmedwassim@hotmail.com>
2022-12-14 17:47:09 -0800

    Even more code

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - Prints sign of intger
+ * @n: integer to be evaluated
+ *
+ * Return: 0 if zero - 1 if postive - -1 if negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		__putchar('+');
+		return (1);
+	}
+       	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+/**
+ * _abs - return absolute of number
+ *
+ * @n: Integer to be evaluated
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-1*n);
+	return (n);
+}
