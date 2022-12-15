e095f3236257dd517158a12d371493ecfd9f0f19
ProbablyOmar <omar_ahmedwassim@hotmail.com>
2022-12-14 17:52:21 -0800

    Even more code

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,7 +9,7 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		__putchar('+');
+		_putchar('+');
 		return (1);
 	}
        	else if (n < 0)
--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,11 +1,12 @@
 /**
  * _abs - return absolute of number
- *
  * @n: Integer to be evaluated
+ *
+ * Return: Returns absolute value of n
  */
 int _abs(int n)
 {
 	if (n < 0)
-		return (-1*n);
+		return (-1 * n);
 	return (n);
 }
