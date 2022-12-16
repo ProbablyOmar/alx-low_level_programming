d4f62ad435a99095dbc26cc067b5b11e1b0728b3
ProbablyOmar <omar_ahmedwassim@hotmail.com>
2022-12-16 17:35:15 +0200

    Square Code

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Fizzbuzz game
+ *
+ * Return: Void
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+		if (!(i % 15))
+			printf("FizzBuzz ");
+		else if (!(i % 3))
+			printf("Fizz ");
+		else if (!(i % 5))
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+	}
+
+	return (0);
+}
