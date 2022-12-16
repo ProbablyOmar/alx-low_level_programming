4eb1d82868a43fcc1baa62b74d34511ba94abda2
ProbablyOmar <omar_ahmedwassim@hotmail.com>
2022-12-16 18:04:40 +0200

    Square Code

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -13,12 +13,19 @@ void print_triangle(int size)
 	int j;
 	int k;
 
-	for (i = 0; i < size; i++)
+	if (size > 0)
 	{
-		for (j = 1; j < size - i; j++)
-			_putchar(' ');
-		for (k = 0; k <= i; k++)
-			_putchar('#');
+		for (i = 0; i < size; i++)
+		{
+			for (j = 1; j < size - i; j++)
+				_putchar(' ');
+			for (k = 0; k <= i; k++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+	else
+	{
 		_putchar('\n');
 	}
 }
