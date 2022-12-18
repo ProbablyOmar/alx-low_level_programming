9ebc01c7f444e01baaeada31bfc9520d38cae15b
ProbablyOmar <omar_ahmedwassim@hotmail.com>
2022-12-18 14:22:03 +0200

    Advanced Task

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints sum of multiples of 5 or 3
+ *
+ * Return: Always 0
+*/
+
+int main(void)
+{
+	int i;
+	int sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (!(i % 3) || !(i % 5))
+			sum += i;
+	}
+	printf("%d\n", sum);
+}
