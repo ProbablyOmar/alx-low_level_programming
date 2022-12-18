042c5643f2b7802d559a625ccf3865d8723c2df3
ProbablyOmar <omar_ahmedwassim@hotmail.com>
2022-12-18 15:06:11 +0200

    Advanced Task

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -12,6 +12,7 @@ int main(void)
 	unsigned long j = 1;
 	int _;
 	unsigned long temp;
+	unsigned long sum = 0;
 
 	printf("1, 2, ");
 	for (_ = 0; _ < 48; _++)
@@ -20,10 +21,8 @@ int main(void)
 		i = j + i;
 		j = temp;
 		if (!(i % 2))
-			printf("%lu", i);
-		if (_ != 47)
-			printf(", ");
+			sum += i;
 	}
-	printf("\n");
+	printf("%lu\n", sum);
 	return (0);
 }
