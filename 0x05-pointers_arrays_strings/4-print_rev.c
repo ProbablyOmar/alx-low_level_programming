ffe14adb02dffdad07529e1ff1c78fcc0b1b8714
ProbablyOmar <omar_ahmedwassim@hotmail.com>
2022-12-21 21:33:31 +0200

    First pointer

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints the reverse of a string
+ * @s: Pointer to String
+ *
+ * Return: Void
+*/
+
+void print_rev(char *s)
+{
+	char *start = s;
+
+	while (*(s + 1) != '\0')
+	{
+		s++;
+	}
+
+	while((s + 1) != start)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
