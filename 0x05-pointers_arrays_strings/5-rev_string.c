b7b546def6f0be2a2c031110cc17a71a1506030d
ProbablyOmar <omar_ahmedwassim@hotmail.com>
2022-12-21 22:10:25 +0200

    First pointer

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,33 @@
+/**
+ * rev_string - Reverses a String
+ * @s: Pointer on a string
+ *
+ * Return: Void
+*/
+
+void rev_string(char *s)
+{
+	if (*s != '\0')
+	{
+		char *start = s;
+		char temp;
+		
+		while (*(s) != '\0')
+		{
+			s++;
+		}
+
+		while (start != s)
+		{
+			temp = *start;
+			*start = *s;
+			*s = temp;
+			start++;
+			if (start != s)
+			{
+				s--;
+			}
+		}
+
+	}
+}
