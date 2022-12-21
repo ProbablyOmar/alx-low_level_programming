c58aa29156c2a9826a5624a4313d8929f7634fe9
ProbablyOmar <omar_ahmedwassim@hotmail.com>
2022-12-21 23:36:15 +0200

    First pointer

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
 /**
- * print_array - prints the first n elements of 
+ * print_array - prints the first n elements of
  * an array
  * @a: ptr to array
  * @n: number of elements
  *
  * Return: void
-*/
+ */
 
 void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n && *a != ; i++)
 	{
-		printf("%d", *a);
+		printf("%d", *(a + i);
 		if (i != n - 1)
 			printf(", ");
 	}
