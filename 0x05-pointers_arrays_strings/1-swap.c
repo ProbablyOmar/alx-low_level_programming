39b67d7bfc4ff26909aa576b31611fb64d51a0ee
ProbablyOmar <omar_ahmedwassim@hotmail.com>
2022-12-21 21:03:49 +0200

    First pointer

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+/**
+ * 1-swap - Swaps values of two variables
+ * @a: First variable
+ * @b: Second variable
+ *
+ * Return: Void
+*/
+
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+
+	*a = *b;
+	*b = temp;
+}
