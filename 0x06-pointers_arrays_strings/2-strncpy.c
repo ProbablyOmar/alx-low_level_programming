905b9ec5546251621b8b9ed209a813cc6d21459d
ProbablyOmar <omar_ahmedwassim@hotmail.com>
2022-12-22 23:25:15 +0200

    String concat

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,17 +12,12 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 	int m = 0;
-	char *j = dest;
-
-	while (*j != '\0')
-	{
-		m++;
-		j++;
-	}
 
 	for (i = 0; src[i] != '\0' && i < n; i++)
 	{
 		dest[i] = src[i];
 	}
+	for (; i < n; i++)
+		dest[i] = '\0';
 	return (dest);
 }
