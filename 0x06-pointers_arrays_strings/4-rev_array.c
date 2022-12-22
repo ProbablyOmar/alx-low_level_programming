952bd47a03c86de0f2bb50d09e77c81482fef3eb
ProbablyOmar <omar_ahmedwassim@hotmail.com>
2022-12-23 01:43:00 +0200

    New source code

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+/**
+ * reverse_array - Reverse an array
+ * @a: pointer to array
+ * @n: number of elements
+ *
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int j = n - 1;
+	int temp;
+
+	while (i < j)
+	{
+		temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+}
