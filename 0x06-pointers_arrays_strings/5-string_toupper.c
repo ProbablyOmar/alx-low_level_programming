2e8be78b13e9838a59dc15b27894d6152530503f
ProbablyOmar <omar_ahmedwassim@hotmail.com>
2022-12-23 01:53:51 +0200

    New source code

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+/**
+ * string_toupper - turn a string into uppercase
+ * @s: ptr to string
+ *
+ * Return: char ptr to new string
+ */
+
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] > 96 && s[i] < 123)
+			s[i] -= 32;
+		i++;
+	}
+}
