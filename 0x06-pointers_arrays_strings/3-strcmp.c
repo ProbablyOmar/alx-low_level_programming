9b92f77ca1cd2b9175af985e549c2eeb14a4d99f
ProbablyOmar <omar_ahmedwassim@hotmail.com>
2022-12-23 00:26:24 +0200

    String concat

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,7 +1,7 @@
 /**
  * _strcmp - Copies one string to another
- * @dest: Destintation pointer
- * @src: source char pointer
+ * @s2: string 1
+ * @s2: string 2
  *
  * Return: int Positive if bigger
  * Negative if smaller
@@ -13,14 +13,14 @@ int _strcmp(char *s1, char *s2)
 	int i = 0;
 	int j = 0;
 
-	while (s1[i] != '\n' && s2[j] != '\n')
+	while (s1[i] != '\0' && s2[j] != '\0')
 	{
 		if (s1[i] != s2[j])
 			return (s1[i] - s2[j]);
 		i++;
 		j++;
 	}
-	if (s1[i] != '\n' || s2[j] != '\n')
+	if (s1[i] != '\0' || s2[j] != '\0')
 		return (s1[i] - s2[j]);
 	return (0);
 }
