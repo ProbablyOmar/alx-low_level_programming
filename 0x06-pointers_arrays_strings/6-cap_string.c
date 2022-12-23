7363cc2c2202bde18a77c8c4d1b98ca84d781edf
ProbablyOmar <omar_ahmedwassim@hotmail.com>
2022-12-23 02:20:29 +0200

    New source code

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,21 @@
+/**
+ * cap_string - Capatalize all words of a string
+ * @s: ptr to string
+ *
+ * Return: char ptr to new string
+ */
+
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] > 96 && s[i] < 123 && (i = 0 ||
+			!((s[i - 1] > 96 && s[i - 1] < 123)
+			 || (s[i - 1] > 64 && s[i - 1] < 91))))
+			s[i] -= 32;
+		i++;
+	}
+	return (s);
+}
