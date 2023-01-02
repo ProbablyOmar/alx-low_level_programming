cf51bea7f83ae5e6039ec8755369f079f03bc483
ProbablyOmar <omar_ahmedwassim@hotmail.com>
2023-01-02 19:35:01 +0200

    First file

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,12 +5,12 @@
  * @n: Number of bytes to reserve
  *
  * Return: Address to area reserved
-*/
+ */
 
 char *_memset(char *s, char b, unsigned int n)
 {
 	int i;
-	
+
 	for (i = 0; i < n; i++)
 	{
 		*s = b;
