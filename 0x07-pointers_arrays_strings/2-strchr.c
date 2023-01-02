82a4ae79f1ec3f973c37206cbfb7114707b3f912
ProbablyOmar <omar_ahmedwassim@hotmail.com>
2023-01-02 20:24:59 +0200

    Yes

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,17 @@
+/**
+ * _strchr - searches for character in string
+ * @dest: Address to string ton be searched
+ * @c: Target character
+ *
+ * Return: Address to first occurace of c
+ */
+
+char *_strchr(char *s, char c)
+{
+	while(*s)
+	{
+		if *s == c
+			return (s);
+		s++;
+	}
+}
