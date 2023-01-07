a0457a728291d9998b04c06e048c360878cec1cf
ProbablyOmar <omar_ahmedwassim@hotmail.com>
2023-01-07 16:07:09 +0200

    Recursion project

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,6 +1,6 @@
 /**
  * factorial- Returns factorial of a number
- * @s: Character Pointer
+ * @n: number
  *
  * Return: Factorial of number
  */
@@ -9,7 +9,7 @@ int factorial(int n)
 {
 	if (n > 0)
 	{
-		return ( n * factorial(n - 1));
+		return (n * factorial(n - 1));
 	}
 	else if (n == 0)
 	{
