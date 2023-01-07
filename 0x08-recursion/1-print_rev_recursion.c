7c9ec560e03bec5c2953400cc10eb5eb37086ec2
ProbablyOmar <omar_ahmedwassim@hotmail.com>
2023-01-07 15:35:51 +0200

    Recursion project

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts_rev_recursion - Prints a string in reverse recursively
+ * @s: Character Pointer
+ *
+ * Return: Void
+ */
+
+void _puts_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_puts_recursion(++s);
+		_putchar(*s);
+	}
+}
