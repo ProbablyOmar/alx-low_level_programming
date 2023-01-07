20634c0344bd57358e7f768e95a1b4afefa21f05
ProbablyOmar <omar_ahmedwassim@hotmail.com>
2023-01-07 17:05:53 +0200

    Recursion project

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,23 @@
+/**
+ * checker - checks the input number from n to the base
+ * @n: number is squared and compared against base
+ * @base: base number to check
+ * Return: natural square root of number base
+ */
+int checker(int n, int base)
+{
+	if (base % n == 0 && n != base)
+		return (1);
+	if (n >= base)
+		return (0);
+	return (checker(n + 1, base));
+}
+/**
+ * is_prime_number - return the natural square root of a number n.
+ * @n: number to check for prime
+ * Return: is it prime for not
+ */
+int is_prime_number(int n)
+{
+	return (checker(2, n));
+}
